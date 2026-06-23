413899cc0e3c5803ffbfebff4b9e3074203fbf73
samriddhipurohit10 <samriddhipurohit111@gmail.com>
2026-06-23 23:26:29 +0530

    Add files via upload

---
+++ fun_simulation.f
@@ -0,0 +1,11 @@
+-gui
+../testbench/tb_sliding_4x4_wrapper.v
+../rtl_7/mac.v
+../rtl_7/top_wrapper.v
+-v /tools/sclpdk_new/SCLPDK_V3.0_KIT/scl180/stdcell/fs120/6M1IL/verilog/vcs_sim_model/tsl18fs120_scl.v
+-v /tools/sclpdk_new/SCLPDK_V3.0_KIT/scl180/iopad/cio150/6M1L/verilog/tsl18cio150/zero/pc3c01.v 
+-v /tools/sclpdk_new/SCLPDK_V3.0_KIT/scl180/iopad/cio150/6M1L/verilog/tsl18cio150/zero/pc3d01.v 
+-v /tools/sclpdk_new/SCLPDK_V3.0_KIT/scl180/iopad/cio150/6M1L/verilog/tsl18cio150/zero/pc3o01.v
+-timescale 1ns/1ps
+-mess
+-access +rwc

8acf433fc5d093e66509d930916fbc4eba1a4516
samriddhipurohit10 <samriddhipurohit111@gmail.com>
2026-06-23 23:34:27 +0530

    Add files via upload

---
+++ fun_simulation.tcl
@@ -0,0 +1,15 @@
+-gui
+../testbench/tb_sliding_4x4_post_synthesis_wrapper_3.v
+-v ../2_Synthesis_wrapper_7_wrapper_6M1L_2/top_streamed_wrapper_chip_netlist.v
+-v ../testbench/ss_verilog.v
+-v ../testbench/ss_verilog_udp.v
+-v /tools/sclpdk_new/SCLPDK_V3.0_KIT/scl180/stdcell/fs120/6M1IL/verilog/vcs_sim_model/tsl18fs120_scl.v
+-v /tools/sclpdk_new/SCLPDK_V3.0_KIT/scl180/iopad/cio150/6M1L/verilog/tsl18cio150/zero/pc3c01.v 
+-v /tools/sclpdk_new/SCLPDK_V3.0_KIT/scl180/iopad/cio150/6M1L/verilog/tsl18cio150/zero/pc3d01.v 
+-v /tools/sclpdk_new/SCLPDK_V3.0_KIT/scl180/iopad/cio150/6M1L/verilog/tsl18cio150/zero/pc3o01.v
+-relax
+-timescale 1ns/1ps
++sdf_verbose 
++maxdelays
+-mess
+-access +rwc
